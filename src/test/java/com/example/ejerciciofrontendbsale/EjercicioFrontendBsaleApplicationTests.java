f5a8647d3045f2da1fcafbb11bbcfad5d421e570
Jose Obreque Fuentes <j.obreque.fuentes@gmail.com>
2022-09-02 12:15:58 -0400

    Commit inicial

---
+++ src/test/java/com/example/ejerciciofrontendbsale/EjercicioFrontendBsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciofrontendbsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioFrontendBsaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
